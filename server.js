df4a5a95c01eea3b0811ab36be371fe65416e434
Archana <archana41.ar@gmail.com>
2021-07-24 11:54:44 +0530

    inputs

--- server.js
+++ server.js
@@ -1,8 +1,74 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+//const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const PORT = process.env.PORT || 8000;
 
+//const nodemailer = require("nodemailer");
+
+
+const db = knex({
+    client: 'pg',
+  connection: {
+    host: 'localhost',
+    user: 'postgres',
+    password: 'Arch@1',
+    database: 'dc'
+  }
+});
 const app = express();
 
-const PORT = 8000;
+app.use(cors())
+app.use(bodyParser.json());
+
+app.get('/booking', (req,res) =>{
+    db.select().from('booking').then(data => {
+        res.send(data)
+    })
+})
+
+app.post('/booking', (req, res) => {
+    const{
+        fname,
+        lname,
+        email,
+        phone,
+        city,
+        country
+    } = req.body;
+
+   var data = {
+    "fname"  : fname,
+    "lname"  : lname,
+    "email"  : email,
+    "phone"  : phone,
+    "city" : city,
+    "country"  : country
+
+    };
+    db.insert({
+        fname  : fname,
+        lname  : lname,
+        email  : email,
+        phone  : phone,
+        city  : city,
+        country  : country
+    }).into('booking').asCallback(function (err) {
+
+        if(err) {
+            res.status(400).json(err)
+            console.log(err);
+        }
+        else{
+            res.status(200).json(data);
+        }
+    })
+
+    });
+
+
+
 app.get('/', (req, res) => {
     res.send('Server is running')
 });
